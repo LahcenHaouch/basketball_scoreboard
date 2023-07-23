84c2405f756c5c94900a8560be8b506e46e8b9f2
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-07-23 23:44:39 +0200

    Enable user to increase home/guest scores

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ const INITIAL_VALUE = 0;
 
 let homeScore = INITIAL_VALUE;
 let guestScore = INITIAL_VALUE;
+
+const homeScoreElement = document.querySelector("#home-score");
+const guestScoreElement = document.querySelector("#guest-score");
+
+document.querySelectorAll(".score-actions.home button").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const num = Number.parseInt(event.target.textContent.split("").at(1), 10);
+
+    homeScore += num;
+
+    homeScoreElement.textContent = homeScore;
+  });
+});
+
+document.querySelectorAll(".score-actions.guest button").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const num = Number.parseInt(event.target.textContent.split("").at(1), 10);
+
+    guestScore += num;
+
+    guestScoreElement.textContent = guestScore;
+  });
+});
